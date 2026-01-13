d9711d391b1b83be7439b77dc4a20edf01f3cdbf
poirazis <poirazis@gmail.com>
2026-01-13 22:18:34 +0200

    Bump version to 1.0.1

--- src/Component.svelte
+++ src/Component.svelte
@@ -32,11 +32,11 @@
 {#if !hidden}
   <div class="super-tag {sizeClass}" use:styleable={$component.styles}>
     {#if icon}
-      <i class={icon} />
+      <i class={icon}></i>
     {/if}
     <span class="tag-text">{text ?? ""}</span>
     {#if hideable}
-      <i class={"ri-close-line close"} on:click={() => (hidden = true)} />
+      <i class={"ri-close-line close"} on:click={() => (hidden = true)}></i>
     {/if}
   </div>
 {/if}
@@ -54,19 +54,19 @@
     gap: 0.5rem;
     min-width: 3rem;
   }
-  
+
   .size-small {
     height: 1.25rem;
     padding: 0rem 0.5rem;
     font-size: 10px;
   }
-  
+
   .size-medium {
     height: 1.65rem;
     padding: 0rem 0.75rem;
     font-size: 12px;
   }
-  
+
   .size-large {
     height: 2rem;
     padding: 0rem 1rem;
@@ -80,15 +80,15 @@
     overflow: hidden;
     white-space: nowrap;
   }
-  
+
   .size-small .tag-text {
     font-size: 11px;
   }
-  
+
   .size-medium .tag-text {
     font-size: 13px;
   }
-  
+
   .size-large .tag-text {
     font-size: 15px;
   }
@@ -101,15 +101,15 @@
       color: var(--spectrum-global-color-gray-900);
     }
   }
-  
+
   .size-small .close {
     font-size: 9px;
   }
-  
+
   .size-medium .close {
     font-size: 11px;
   }
-  
+
   .size-large .close {
     font-size: 13px;
   }
